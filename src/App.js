8019fd95b10ecbe7524a67b689fd4c8fd1e1ab81
Pham Huu An <phaman3899@gmail.com>
2021-09-09 08:18:28 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStudent = (initStudents) => {
+	const [students] = useState(initStudents);
+
+	const getStudentIndex = (name) => {
+		return students.indexOf(name);
+	};
+
+	return [getStudentIndex];
+
+}
+
+const App = () => {
+	const [names] = useState(['Huyen', 'Hoa', 'Hung', 'Long']);
+	const [getStudentIndex] = useStudent(names);
+	const [value, setValue] = useState('');
+	const [result, setResult] = useState('');
+
+	const handleChange = (event) => {
+		setValue(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const index = getStudentIndex(value);
+		setResult(index === -1 ? 'NaN' : `${index + 1}`);
+	};
+
+	return (
+		<div>
+			<div>学生一覧：{names.join(', ')}</div>
+			<div>検索名前：<input value={value} onChange={handleChange} /></div>
+			<button onClick={handleSubmit}>確定</button>
+			<div>検索名前：{value}</div>
+			<div>位置：{result}</div>
+		</div>
+	);
 }
 
 export default App;
